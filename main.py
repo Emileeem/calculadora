9d5befe05971aaabda4ad78082d3ddf56ae69078
Emyli Quadros <euemyliquadros@gmail.com>
2023-03-21 09:42:20 -0300

    Conta_Soma_Mult

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from emyli import *
 
 conta = int(input('O que você deseja fazer?\n1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n: '))
 n1 = int(input('Digite o primeiro número: '))
@@ -5,7 +6,7 @@ n2 = int(input('Digite o segundo número: '))
 
 while True:
     if conta == 1:
-        result = emyli.sum(n1,n2)
+        result = emyli.soma(n1,n2)
         break
     elif conta == 2:
         result = benhur.sub(n1,n2)
