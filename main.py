be2c1e4a7e150c3b75a74d8bb5f84d0b1b9ad1ed
Emyli Quadros <euemyliquadros@gmail.com>
2023-03-21 09:32:34 -0300

    Adicionando_mensagem_usuario_main

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-conta = int(input(''))
-n1 = int(input(''))
-n2 = int(input(''))
+conta = int(input('O que você deseja fazer?\n1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n: '))
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
