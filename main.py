bed631b3cf6eab52a68480885f4fcfb5c388db4f
Benhur Feld <benhurfeld@gmail.com>
2023-03-21 10:38:47 -0300

    alteracao_import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from emyli import *
-from benhur import *
+import emyli, benhur
+
 conta = int(input('O que você deseja fazer?\n1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n: '))
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
